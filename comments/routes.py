93d3ad652b15d8949f49e557cb6bd807ca4c85c8
datashaman <marlinf@datashaman.com>
2020-06-21 16:01:30 +0200

    refactor

--- comments/routes.py
+++ comments/routes.py
@@ -1,12 +1,12 @@
-from flask_resty import Api
+import flask_resty
+flask_resty.api.DEFAULT_ID_RULE = '<int:id>'
 
 from . import app, views
 
-api = Api(app)
-
-api.add_resource('/comments/', views.CommentListView, views.CommentView, id_rule='<int:id>')
-api.add_resource('/sites/', views.SiteListView, views.SiteView, id_rule='<int:id>')
-api.add_resource('/topics/', views.TopicListView, views.TopicView, id_rule='<int:id>')
-api.add_resource('/users/', views.UserListView, views.UserView, id_rule='<int:id>')
+api = flask_resty.Api(app)
 
 api.add_ping('/ping/')
+api.add_resource('/comments/', views.CommentListView, views.CommentView)
+api.add_resource('/sites/', views.SiteListView, views.SiteView)
+api.add_resource('/topics/', views.TopicListView, views.TopicView)
+api.add_resource('/users/', views.UserListView, views.UserView)
